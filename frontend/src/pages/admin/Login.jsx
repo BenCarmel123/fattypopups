31a6abec44f74b860fa5657c0ef35c0aa8f4ffb7
BenCarmel123 <Bencarmel1@mail.tau.ac.il>
2025-12-24 23:43:12 +0200

    Finalize login button UI

--- frontend/src/pages/admin/Login.jsx
+++ frontend/src/pages/admin/Login.jsx
@@ -1,25 +1,30 @@
-import { MINIMAL_TRANSITION, CENTER, LARGE, XL, XXL, MAX, SMALL } from "../../components/config/strings.jsx";
-import { WHITE, BORDER_COLOR, HOVER, GRAY, BACKGROUND_COLOR } from "../../components/config/colors.jsx";
-import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
+import { MINIMAL_TRANSITION, CENTER, APP_NAME, XL, XXL, MAX, MEDIUM, LARGE} from "../../components/config/strings.jsx";
+import { WHITE, BORDER_COLOR, HOVER, GRAY, EVENT_TITLE_PADDING_COLOR } from "../../components/config/colors.jsx";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { FcGoogle } from "react-icons/fc";
 
 const handleGoogle = () => {
   window.location.href = "http://localhost:5000/auth/google";
 };
 
+const LOGO_URL = process.env.REACT_APP_LOGO;
+
 export default function Login() {
   return (
-    <Flex minH="50vh" align={CENTER} justify={CENTER} px={4}>
-      <Box w={MAX} maxW={LARGE} bg={WHITE} rounded={XXL} shadow={XL} p={{ base: 8, md: 12 }} border="1px solid" borderColor={BORDER_COLOR}>
-        <Box textAlign={CENTER} mb={2}>
-        </Box>
-
-        <Button onClick={handleGoogle} variant="outline" w={MAX} rounded={XL} borderColor={HOVER} _hover={{ bg: "gray.50", borderColor: "gray.300" }} transition={MINIMAL_TRANSITION}>
+    <Flex minH="90vh" align={CENTER} justify={CENTER} px={10}>
+      <Box bg={WHITE} rounded={XXL} shadow={XL} p={6} border="3px solid" borderColor={BORDER_COLOR}>
+        <div direction="column"
+        align={CENTER}
+        justify={CENTER}>
+        <img src={LOGO_URL} alt={APP_NAME} className="h-42 w-40" style={{width:'9rem', backgroundColor:HOVER, borderRadius:"16px", padding:"10px"}} />
+        <br></br>
+        <Button onClick={handleGoogle} variant="outline" padding="0.8rem" size={LARGE} rounded={XXL} border="3px solid" borderColor={BORDER_COLOR} _hover={{ bg: HOVER }} transition={MINIMAL_TRANSITION}>
           <FcGoogle />
-            <Text fontWeight={LARGE} color={GRAY} fontSize={XXL}>
-            Continue with Google
+          <Text fontWeight="semibold" color={GRAY} fontSize={LARGE}>
+            Admin
           </Text>
         </Button>
+        </div>
       </Box>
     </Flex>
   );
