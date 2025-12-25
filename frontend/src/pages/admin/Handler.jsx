e04ea61b25bac2818c5474159107c5e11e4c9657
BenCarmel123 <Bencarmel1@mail.tau.ac.il>
2025-12-25 14:37:26 +0200

    Improve Admin Handler handling of token

--- frontend/src/pages/admin/Handler.jsx
+++ frontend/src/pages/admin/Handler.jsx
@@ -7,35 +7,49 @@ import Login from "./Login.jsx";
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 export default function AdminPageHandler() {
+  // Component State
   const[isAuthenticated, setAuthenticated] = useState(false)
   const[action, setAction] = useState(LOGIN);
   const[selectedEvent, setSelectedEvent] = useState(undefined);
 
+  const handleClick = (action, selectedEvent) => () => {
+    setAction(action);
+    setSelectedEvent(selectedEvent || undefined); 
+  };
+
+  // On Mount
   useEffect(() => {
+  // Check if token in URL
   const params = new URLSearchParams(window.location.search);
   const tokenFromUrl = params.get("token");
 
-  console.log("[DEBUG] Token from URL:", tokenFromUrl);
-
+ // Backend Sent Token
   if (tokenFromUrl) {
+    console.log("[DEBUG] Token from URL:", tokenFromUrl);
     localStorage.setItem("auth_token", tokenFromUrl);
     window.history.replaceState({}, "", window.location.pathname);
   }
 
+  // Verify Token
   console.log("[DEBUG] Token in storage:", localStorage.getItem("auth_token"));
-
   const token = localStorage.getItem("auth_token");
-  if (!token) return;
+
+  // No Token
+  if (!token) {
+    setAuthenticated(false)
+    setAction(LOGIN)
+    return
+  }
+
+  // Yes Token
   console.log("[DEBUG] pending token to /auth/check");
+
   fetch(`${SERVER_URL}/auth/check`, { headers: { Authorization: `Bearer ${token}`,},})
       .then(res => res.json()).then(data => {
+          if (!data.authenticated) localStorage.removeItem("auth_token");
         setAuthenticated(data.authenticated);
         setAction(data.authenticated ? DASHBOARD : LOGIN);});
   }, []);
-
-  const handleClick = (action, selectedEvent) => () => {
-    setAction(action);
-    setSelectedEvent(selectedEvent || undefined); };
 
    switch(action) {
          case ADD:
