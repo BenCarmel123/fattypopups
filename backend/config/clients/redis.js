dd16518863ecaca594b16b14aa199ad87414e585
BenCarmel123 <Bencarmel1@mail.tau.ac.il>
2026-03-14 21:02:57 +0200

    FAT-33
    Make Redis optional with graceful degradation when REDIS_URL is not set

--- backend/config/clients/redis.js
+++ backend/config/clients/redis.js
@@ -1,7 +1,9 @@
 import Redis from 'ioredis';
 import { logger } from "../../utils/logger.js";
 
-export const redis = new Redis({ host: 'redis', port: 6379 });
+export const redis = process.env.REDIS_URL ? new Redis(process.env.REDIS_URL) : null;
 
-redis.on('error', err => logger.error("[Redis] Redis Client Error - " + err));
+if (redis) {
+  redis.on('error', err => logger.error("[Redis] Redis Client Error - " + err));
+}
 
