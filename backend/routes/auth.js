82f63c046caa7a40b39301193c83dc3d0b25be1b
BenCarmel123 <Bencarmel1@mail.tau.ac.il>
2026-04-25 04:59:47 +0300

    [FAT-93] - add blank lines between routes in auth router

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,9 @@ import * as authController from '../controllers/authController.js';
 const authRouter = express.Router();
 
 authRouter.get('/google', authController.redirectToGoogle);
+
 authRouter.get('/google/callback', authController.handleGoogleCallback);
+
 authRouter.get('/check', authController.checkAuth);
 
 export default authRouter;
